5e5ebd47f52401237a43f17d9df1a388c4232650
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-23 22:36:00 +0000

    Add push, pop, pint and pall

--- opcode.c
+++ opcode.c
@@ -32,12 +32,11 @@ void push(stack_t **stack, unsigned int line_number)
 /**
  * pall - prints all values on the stack, starting from top.
  * @stack: the stack being printed.
- * @line_number: the value of the element.
+ * @line_number: the line number.
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-	(void) line_number;
 
 	if (*stack == NULL)
 		return;
@@ -54,15 +53,17 @@ void pall(stack_t **stack, unsigned int line_number)
 /**
  * pint - prints the value at the top of the stack
  * @stack: the stack that is given
- * @line_number: the value of the element.
+ * @line_number: the line number.
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	(void) line_number;
 	stack_t *tmp;
 
-	if(*stack == NULL)
-		return;
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	tmp = *stack;
 
@@ -72,22 +73,24 @@ void pint(stack_t **stack, unsigned int line_number)
 /**
  * pop - removes the top element of the stack.
  * @stack: the stack that holds the elements.
- * @line_number: the value of the element.
+ * @line_number: the line number.
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-	(void) line_number;
 
-	if (*stack == NULL)
-		return;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	tmp = *stack;
 
 	if (tmp->next != NULL)
 	{
-		stack = tmp->next;
-		stack->prev = NULL;
+		*stack = tmp->next;
+		(*stack)->prev = NULL;
 		free(tmp);
 	}
 	else
