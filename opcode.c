188cecdf6be8a3a729b72adeee2a7e3dcfa5d1b9
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-24 13:16:02 +0000

    Exit message

--- opcode.c
+++ opcode.c
@@ -37,9 +37,13 @@ void push(stack_t **stack, unsigned int line_number)
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
+	(void)line_number;
 
 	if (*stack == NULL)
-		return;
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	tmp = *stack;
 
