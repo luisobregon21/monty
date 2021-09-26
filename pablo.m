133c95fa7cd0886e0475722c9f91cc26361656e8
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-26 15:56:11 +0000

    Updated test directory

--- pablo.m
+++ pablo.m
@@ -1,1 +1,12 @@
-pint
+push 0
+push 1
+push 2
+  push 3
+                   pall    
+
+push 4
+
+    push 5    
+      push    6        
+
+pall
---
+++ testing.m
@@ -0,0 +1,97 @@
+push 1
+push 2
+push 3
+pall
+
+
+################
+push 1
+push     2
+
+push
+#########
+push 1
+push     2
+
+
+    push       1024
+push
+##########i
+push 4
+pushe 3
+##############
+push 1
+push 1024
+push -0
+push -1
+push -1024
+push 123a
+############
+pall
+push 1
+pall
+push 2
+pall
+push 3
+pall
+push 4
+pall
+#############
+push 1
+push     2
+
+
+    push       1024
+push
+########
+pall
+push 1
+pall
+push 2
+pall
+push 3
+pall
+push 4
+pall hello! :) we don't really care about what's after pall
+#################
+push 0
+push 1
+push 2
+  push 3
+                   pall    
+
+push 4
+
+    push 5    
+      push    6        
+
+pall
+############
+push 0 Push 0 onto the stack
+push 1 Push 1 onto the stack
+
+push 2
+  push 3
+                   pall
+
+
+
+push 4
+
+    push 5
+      push    6
+
+pall This is the end of our program. Monty is awesome!
+#################
+push 0
+push 1
+push 2
+  push 3
+                   pall
+
+push 4
+
+    push 5
+      push    6
+
+pall
